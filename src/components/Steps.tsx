2837168d11d730e973584172dbf0496a61a84773
AyushBherwani1998 <ayush.bherwani1998@gmail.com>
2025-09-07 12:44:48 +0400

    initial commit

---
+++ src/components/Steps.tsx
@@ -0,0 +1,44 @@
+
+import TransactionForm from "@/components/TransactionForm";
+import useSmartAccount from "@/hooks/useSmartAccount";
+import { useAccount } from "wagmi";
+
+export default function Steps() {
+  const { isConnected } = useAccount();
+  const { smartAccount } = useSmartAccount();
+
+  if (!isConnected) {
+    return (
+      <div className="max-w-2xl mx-auto flex flex-col gap-8">
+        <p className="text-white/70 leading-relaxed">
+          The first step is to connect your wallet.
+          <br />
+          <br />
+          You can customize the Wagmi configuration to connect to any chain
+          supported by the MetaMask Smart Accounts, and use any connector you
+          prefer. The connected wallet will serve as the signer for your smart
+          account.
+        </p>
+      </div>
+    );
+  } else {
+    return (
+      <div className="max-w-2xl mx-auto">
+        <p className="text-white/70 leading-relaxed">
+          Once you have connected your wallet, you can send a user operation
+          from the smart account.
+          <br />
+          <br />
+          The smart account will remain counterfactual until the first user
+          operation. If the smart account is not deployed, it will be
+          automatically deployed upon the sending first user operation.
+          <br />
+          <br />
+          This is your smart account address {smartAccount?.address} make sure to deposit funds to it
+          before sending transactions.
+        </p>
+        <TransactionForm />
+      </div>
+    );
+  }
+}
